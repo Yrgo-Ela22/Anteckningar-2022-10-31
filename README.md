0649f6b5a2debc17325b7c2791e1a227996efc23
Erik Pihl <100573213+ErikPihl@users.noreply.github.com>
2022-10-31 21:47:28 +0100

    Laddade upp filer

---
+++ README.md
@@ -0,0 +1,7 @@
+# Anteckningar 2022-10-31
+Implementering av strukt för lysdioder och andra digitala utportar, implementerat på mikrodator ATmega328P.
+
+Filen "Strukten room.zip" utgör en repetitionsövning för struktar, där en strukt för lagring, 
+beräkning samt utskrift av längd, bredd och höjd för ett rum implementeras.
+
+Resterande filer utgör ett projekt där en strukt döpt led implementeras för enkel initiering och styrkning av lysdioder.
---
+++ main.c
@@ -0,0 +1,22 @@
+/********************************************************************************
+* main.c: Demonstration av datastruktur för enkel styrning av lysdioder.
+********************************************************************************/
+#include "led.h"
+
+/********************************************************************************
+* main: Ansluter en lysdiod till pin 8 / PORTB0. Denna lysdiod sätts till att
+*       blinka kontinuerligt var 100:e millisekund.
+********************************************************************************/
+int main(void)
+{
+   struct led l1;
+   led_init(&l1, 8);
+   
+   while (1)
+   {
+      led_blink(&l1, 100);
+   }
+
+   return 0;
+}
+
